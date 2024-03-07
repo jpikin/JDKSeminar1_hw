c3e5e6be405f045f7f32ef47cee73f6250ecb664
Pikin <pikin@bk.ru>
2024-03-07 17:20:11 +0500

    log class fixed method readLogs

--- src/app/Logs.java
+++ src/app/Logs.java
@@ -1,34 +1,32 @@
 package app;
 
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class Logs {
 
     /**
-     * Метод читает файл с логами.
+     * Метод читает и возвращает файл с логами.
      */
-    public static String readLogs(){
-        String txt = "";
-        try {
-            File loadFile = new File("./src/logs/logs.txt");
-            FileReader reader = new FileReader(loadFile);
-            txt = reader.toString();
+    public static String readLogs() {
+        StringBuilder txt = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader("./src/logs/logs.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                txt.append(line+'\n');
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        catch (IOException e){
-            Server.chat.setText("Ошибка при загрузке лога");
-        }
-        return txt;
+        return txt.toString();
     }
 
     /**
      * Метод записывает текущее сообщение в файл с логами.
+     *
      * @param txt Текущее сообщение.
      */
-    public static void saveLogs(String txt){
+    public static void saveLogs(String txt) {
         try {
             File saveFile = new File("./src/logs/logs.txt");
             FileWriter writer = new FileWriter(saveFile, true);
--- src/app/Server.java
+++ src/app/Server.java
@@ -30,7 +30,7 @@ public class Server extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 isStarted = true;
-                String readLogs = Logs.readLogs();
+                chat.setText(Logs.readLogs());
             }
         });
         btnStop.addActionListener(new ActionListener() {
